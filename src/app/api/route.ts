16c1699e86e8c96030a8b006a7d7971e29098b5e
Haardik <hhaardik@uwaterloo.ca>
2023-06-21 10:23:18 -0400

    fix

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,31 +1,25 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { Configuration, OpenAIApi } from 'openai';
-import fs from 'fs'
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-const openai = new OpenAIApi(configuration);
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest, res: NextResponse) {
-  console.log("hello there")
-  // console.log(await req.json())
   const data = await req.formData();
   const theFile: File | null = data.get("file") as unknown as File;
 
-  console.log(data)
-  console.log(theFile)
+  const formData = new FormData();
+  formData.append("file", theFile);
+  formData.append("model", "whisper-1");
 
+  const response = await fetch(
+    "https://api.openai.com/v1/audio/transcriptions",
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+      body: formData,
+    }
+  );
 
+  const body = await response.json();
 
-  
-  const resp = await openai.createTranscription(
-    theFile,
-    "whisper-1"
- );
-
-//  console.log(resp)
-  return NextResponse.json({ output: "resp" }, { status: 200 })
-
-};
+  return NextResponse.json({ output: body });
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,53 @@
 "use client";
-import { ChangeEvent, useState } from "react";
+import React, { useState } from "react";
 
 export default function Home() {
-  const [theFile, setTheFile] = useState<File>()
-  const [isLoading, setIsLoading] = useState(false)
-  const [response, theResponse]  = useState("")
+  const [theFile, setTheFile] = useState<File | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [response, setResponse] = useState("");
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.currentTarget.files?.[0];
+    if (!file) return;
+
     setTheFile(file);
   };
 
   const callGetTranscription = async () => {
     setIsLoading(true);
-    
-    console.log("doing that")
+
     if (!theFile) {
       // Handle the case when no file is selected
       setIsLoading(false);
       return;
     }
-  
+
     const formData = new FormData();
-    formData.append('file', theFile);
-  
+    formData.set("file", theFile);
+
     try {
-      const response = await fetch('/api', {
-        method: 'POST',
+      const response = await fetch("/api", {
+        method: "POST",
         body: formData,
       });
-  
+
       if (response.ok) {
         // Handle the success response
-        console.log('File uploaded successfully');
+        console.log("File uploaded successfully");
       } else {
         // Handle the error response
-        console.error('Failed to upload file');
+        console.error("Failed to upload file");
       }
+
+      const data = await response.json();
+
+      setResponse(data.output.text);
     } catch (error) {
       // Handle any errors
-      console.error('An error occurred while uploading the file', error);
+      console.error("An error occurred while uploading the file", error);
     }
-  
-    setTheFile(undefined);
+
+    setTheFile(null);
     setIsLoading(false);
   };
 
@@ -52,21 +57,21 @@ export default function Home() {
 
       <div className="flex  h-[35rem] w-[40rem] flex-col items-center bg-gray-600 rounded-xl">
         <div className=" h-full flex flex-col gap-2 overflow-y-auto py-8 px-3 w-full">
-          <input type="file"   accept=".wav, .mp3"  onChange={handleFileChange}/>
-  
-          <div className="w-[90%] h-max border-2 break-words">dsioafjiosdfjoisajfoa dsijfiosdjfiosajfiojfoiasjfoisjfoisjfiosjfsiopfjsiodpfjoipsdsiofjsaoidfjisdofjspodf</div>
+          <input type="file" accept=".wav, .mp3" onChange={handleFileChange} />
 
+          <div className="w-[90%] h-max border-2 break-words">
+            {isLoading ? "Loading..." : response ? response : ""}
+          </div>
         </div>
         <div className="relative  w-[80%] bottom-4 flex justify-center">
           <button
             onClick={callGetTranscription}
             className="w-max bg-blue-500 px-4 py-2 rounded-sm "
-          >Upload
+          >
+            Upload
           </button>
         </div>
       </div>
-
-      <div></div>
     </main>
   );
 }
